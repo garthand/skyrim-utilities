43dafc0bf70163addcb582e9e8338e71ded22d3b
garthand <garthand@users.noreply.github.com>
2018-12-18 20:09:36 -0600

    Initial upload
    Clones one language's string files and renames them for the supported languages so you don't have to do so manually

---
+++ generate_strings.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+strings=$(echo ./*.strings)
+dlstrings=$(echo ./*.dlstrings)
+ilstrings=$(echo ./*.ilstrings)
+chmod 700 "$strings" "$dlstrings" "$ilstrings"
+languages=(French Spanish German Russian Polish Italian)
+basename=$(awk -F '_' '{print $1}' <<< "$strings")
+for language in "${languages[@]}"; do
+    cp "$strings" "$basename""_""$language"".strings"
+    cp "$dlstrings" "$basename""_""$language"".dlstrings"
+    cp "$ilstrings" "$basename""_""$language"".ilstrings"
+done
